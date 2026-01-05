54c8d5666f885022730996752692ce2d1a3b3d6e
Yash2416f <yash.gupta.09032004@gmail.com>
2026-01-05 22:49:02 +0530

    Added the Solution of Valid Anagram and Contain Duplicate

---
+++ Arrays/242_valid_anagram.py
@@ -0,0 +1,9 @@
+#leetcode 242: Valid Anagram
+#https://leetcode.com/problems/valid-anagram/
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if (sorted(s) == sorted(t)):
+            return True
+        else:
+            return False
