de00480dbe15a41a996099d4892e2c230dbd1711
Yash2416f <yash.gupta.09032004@gmail.com>
2026-01-15 22:55:02 +0530

    Added solution of Fibonacci Number

---
+++ DynamicProgramming/509_fibonacci_number.py
@@ -0,0 +1,13 @@
+#leetcode problem 509: Fibonacci Number
+# https://leetcode.com/problems/fibonacci-number/description/
+
+class Solution:
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+
+        a, b = 0, 1
+        for _ in range(1, n):
+            a, b = b, a + b
+
+        return b
